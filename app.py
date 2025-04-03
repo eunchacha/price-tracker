c1d056b0689222507a02e2d37752384c68f53be2
eunchacha <116049833+eunchacha@users.noreply.github.com>
2025-04-03 15:34:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, send_file, render_template_string
+import os  # ← 이거 꼭 필요!
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # HTML을 문자열로 직접 작성해 간단히 보여줍니다
     html = """
     <html>
         <head>
             <title>🎸 가격 추적</title>
         </head>
         <body style="text-align:center; font-family:sans-serif;">
-            <h2>🎸 SchoolMusic 가격 변동 그래프</h2>
+            <h2>🎸 SchoolMusic (데임 Dame PS-5 베이스기타 (TS))가격 변동 그래프</h2>
             <img src="/graph" style="width:80%;">
         </body>
     </html>
@@ -23,5 +23,5 @@ def graph():
     return send_file("static/price_graph.png", mimetype="image/png")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # 환경변수에서 포트 가져오기
     app.run(host="0.0.0.0", port=port)
